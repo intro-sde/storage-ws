3516f0a945cef071df2eda5331dffa871859fe6c
Unknown <julia.hermann93@gmail.com>
2018-01-22 09:51:14 +0100

    Fixing secret

--- src/rest/connection/Connection.java
+++ src/rest/connection/Connection.java
@@ -3,11 +3,9 @@ package rest.connection;
 import com.recombee.api_client.RecombeeClient;
 
 public class Connection {
-	private final static String DB_ID = "sde-final-assignment";
-	private final static String SECRET_TOKEN = "LeDMWIM05dYcFydwyKTxJnJbsfwOE7kieL6Hk0oB9VGqzJ3rYCBjhAcV3AMQhpQo";
-
+	
 	public static RecombeeClient createRecombeeClient() {
-		RecombeeClient client = new RecombeeClient(DB_ID, SECRET_TOKEN);
+		RecombeeClient client = new RecombeeClient(RecombeeToken.DB_ID, RecombeeToken.SECRET_TOKEN);
 		return client;
 	}
 	
